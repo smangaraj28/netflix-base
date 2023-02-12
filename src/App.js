c82c315da98b3b70752eefa12b1106019819174e
soumya sambit mangaraj <smangaraj28@gmail.com>
2023-02-12 10:30:03 +0530

    start from scratch

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "pages/HomePage";
 function App() {
   return (
     <div className="app">
-      <HomePage />
+      Let's make Netflix
     </div>
   );
 }
--- src/ui/MediaSlider.js
+++ src/ui/MediaSlider.js
@@ -3,34 +3,8 @@ import React, { useState, useEffect } from "react";
 import MediaCard from "./MediaCard";
 import tmdbApi from 'api/tmdb';
 
-function MediaSlider({ mediaType, title, path, params = {}, isLarge }) {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const json = await tmdbApi.get(path, params);
-      setItems(json.results);
-    };
-    fetchData();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-
-  return (
-    <div className="media-slider">
-      <h3 className="media-slider__title">{title}</h3>
-      <div className="media-slider__cards">
-        {items.map((media) => (
-          <MediaCard
-            isLarge={isLarge}
-            key={media.id}
-            media={media}
-            mediaType={media.media_type || mediaType}
-          />
-        ))}
-      </div>
-    </div>
-  );
+function MediaSlider() {
+  return null;
 }
 
 export default MediaSlider;
